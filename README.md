69f7cdc3ee4af08366c1b49266694eea329c4174
Alejandro Madriñán <alejandro.madrinanfer@alumnos.upm.es>
2018-04-21 23:30:43 +0200

    input

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # the-clock
-Clock that rewrites the current time
+Clock that rewrites the current time.
+## Sweep time
+The program sweeps over the last time shown with the new time.
+You can choose a certain refresh time for the clock (in ms). Or you could avoid choosing...
+
--- clock.js
+++ clock.js
@@ -1,14 +1,24 @@
+// Input characters interpreted in UTF-8
+process.stdin.setEncoding('utf8');
+
 function getTime() {
     let time = new Date();
     return "Time: " + time.getHours() + "h " + time.getMinutes() + "min "
         + time.getSeconds() + "s "  + time.getMilliseconds() + "ms";
 }
-// Actualización del reloj
-setInterval(
-    function () {
-        process.stdout.write(`\r ${getTime()}`)// El reloj sobreescribe siempre la misma línea
-    },
-    90
-);
 
-console.log("\n        THE CLOCK\n")
+process.stdout.write(" Introduce un tiempo de refresco (en milisegundos): ");
+process.stdin.on('data',function (line) {
+    if (!Number(line)){
+        throw new Error("Introduce un número!!")
+    }
+    // Actualización del reloj
+    setInterval(
+        function () {
+            process.stdout.write(`\r ${getTime()}   `); // El reloj sobreescribe siempre la misma línea
+        },
+        line
+    );
+    console.log("\n        THE CLOCK\n");
+    }
+);
